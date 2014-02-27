6c4a2a951fc0158ae6550ba2bdc3f57e23a012e5
Hooshmand <shokri.hooshmand@gmail.com>
2014-02-27 14:52:18 -0500

    Simulate Constant Degree Revision

--- Experiments/size/Simulate_ConstantDegree.py
+++ Experiments/size/Simulate_ConstantDegree.py
@@ -1,4 +1,3 @@
-
 import numpy.random as random
 from Game import Game
 
@@ -6,16 +5,23 @@ report = open('output.o', 'w')
 
 data = {}
 
-size = 10
-while size < 100:
+size = 50
+while size < 60:
 
-	size += 10
-	data[size] = []
-	for i in range(0, 10):
-		g = Game(size, 0.5, 0.5, 0.3, 0.3)
-		steps = g.playGame()
-		data[size].append(steps)
+        size += 10
+        data[size] = []
+        for i in range(0, 5):
+
+                density = 1
+                if  0.1 * size > 7:
+                        density = 7.0 / (density * 0.1)
+
+                limit = 0.2 
+                g = Game(size, density, limit, 0.3, 0.1)
+                steps = g.playGame()
+                data[size].append(steps)
 
 report.write(str(data))
 
 report.close()
+
